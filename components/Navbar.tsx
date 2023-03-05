7475c39a3fe5c68ac69eded51211fc0786f54930
Ankush523 <ankush.ad050203@gmail.com>
2023-03-05 10:56:06 +0530

    logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,16 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
+import Image from 'next/image'
 import React from 'react'
+import logo from "../images/dca.png"
 const Navbar = () => {
   return (
     <div className='flex flex-row mx-[3%] my-[2%] justify-between'>
       <div className='flex flex-row justify-between'>
-          <div className='pr-[90%]'>
-            <label className='text-lg'>DCA</label>
-          </div>
 
-          <div className='flex flex-row ml-[90%] text-lg'>
+            <Image src={logo} alt="logo" className='w-[40%]'/>
+
+
+          <div className='flex flex-row ml-[80%]  text-lg'>
             <button className='px-[10%] mx-[5%]'>Invest</button>
             <button className='px-[10%] mx-[5%]'>Wrap/Unwrap</button>
           </div>
